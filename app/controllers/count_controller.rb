6b7302d90fb7d84a38a53b5650e9ed695db94261
Cybernetricks <mcgbradley@gmail.com>
2026-01-09 20:50:08 +0000

    Added_new_and_create_actions_for_count

--- app/controllers/count_controller.rb
+++ app/controllers/count_controller.rb
@@ -1,7 +1,20 @@
 class CountController < ApplicationController
   def new
+    @count = Count.new
   end
 
   def create
+    @count = Count.new(count_params)
+
+    if @count.save
+      redirect_to @count
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+    def count_params
+      params.require(:count).permit(:sensorGroup)
+    end
 end
