f35421b9892cd08e1f6b128839298ddd1292d302
Cybernetricks <mcgbradley@gmail.com>
2026-01-11 21:43:28 +0000

    Added_sensor_event_validation_and_relationship

--- app/models/sensor_event.rb
+++ app/models/sensor_event.rb
@@ -2,7 +2,8 @@ class SensorEvent < ApplicationRecord
   belongs_to :room
 
   validates :room, presence: true
-  validates :delta, presence: true
+  validates :delta, presence: true, inclusion: { in: [-1, 1] }
+  validates :recorded_at, presence: true
 
   enum direction: { exit: -1, entry: 1 }
 end
